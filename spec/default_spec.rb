b80ee230a4340739412b6c06e0507cb1b1452d68
wate <pc.poisoning@gmail.com>
2018-03-14 16:19:36 +0900

    テストを修正

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,28 +1,50 @@
 require 'spec_helper'
 
-describe 'role common' do
-  property['common_packages'].each do |pkg|
-    describe package(pkg) do
-      it { should be_installed }
-    end
-  end
+describe file('/etc/skel/public') do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 755 }
+end
 
-  describe package('firewalld') do
-    it { should be_installed }
-  end
-  describe package('etckeeper') do
-    it { should be_installed }
-  end
-  describe package('fail2ban') do
-    it { should be_installed }
-  end
+describe file('/home') do
+  it { should be_mode 755 }
+end
 
-  describe service('firewalld') do
-    it { should be_enabled }
-    it { should be_running }
+describe file('/etc/sudoers.d/wheel') do
+  it { should exist }
+  it { should be_file }
+  if property['sudo_require_password']
+    it { should contain '%wheel ALL=(ALL) ' }
+    it { should_not contain '%wheel ALL=(ALL) NOPASSWD: ALL' }
+  else
+    it { should contain '%wheel ALL=(ALL) NOPASSWD: ALL' }
   end
-  describe service('fail2ban') do
-    it { should be_enabled }
-    it { should be_running }
+end
+
+property['common_packages'].each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
   end
+end
+
+describe package('firewalld') do
+  it { should be_installed }
+end
+
+describe package('etckeeper') do
+  it { should be_installed }
+end
+
+describe package('fail2ban') do
+  it { should be_installed }
+end
+
+describe service('firewalld') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('fail2ban') do
+  it { should be_enabled }
+  it { should be_running }
 end
